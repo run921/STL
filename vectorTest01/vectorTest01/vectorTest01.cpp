5769ad5f924d3a1cb51211a8090023a5b0cd8ee6
gao <gao@qq.com>
2020-06-05 14:40:11 +0800

    Vector Test

--- vectorTest01/vectorTest01/vectorTest01.cpp
+++ vectorTest01/vectorTest01/vectorTest01.cpp
@@ -12,7 +12,7 @@ int main()
     //vector<int> v(a, a + 6);      //也可不写allocator<int>,默认会有分配器
 
     //大于等于40的元素
-    cout << count_if(v.begin(), v.end(), not1(bind2nd(less<int>(), 40)));   //not1为否定，内部表示小于40
+    cout << count_if(v.begin(), v.end(), not1(bind2nd(less<int>(), 40))) << endl;   //not1为否定，内部表示小于40
     //bind2ed绑定第二参数
     //count_if是标准算法
 
